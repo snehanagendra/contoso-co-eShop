77f387dfe303cb99549333add7e42390fb986227
snehanagendra <sneha.nagendra@gmail.com>
2025-11-03 18:52:09 -0800

    fix bicep config path

--- .devcenter/catalog/image-definitions/1es-imaging/write-bicep-config.ps1
+++ .devcenter/catalog/image-definitions/1es-imaging/write-bicep-config.ps1
@@ -1,4 +1,4 @@
 $bicepConfigJson = '{"experimentalFeaturesEnabled":{"desiredStateConfiguration":true}}'
-$bicepConfigPath = "C:\ProgramData\Microsoft\DevBoxAgent\ImageDefinitions\1es-imaging-catalog-01\1es-imagingbicepconfig.json"
-New-Item -Path "C:\Workspaces\trydsc" -ItemType Directory -Force | Out-Null
+$bicepConfigPath = "C:\ProgramData\Microsoft\DevBoxAgent\ImageDefinitions\1es-imaging-catalog-01\1es-imaging\bicepconfig.json"
+New-Item -Path (Split-Path $bicepConfigPath -Parent) -ItemType Directory -Force | Out-Null
 Set-Content -Path $bicepConfigPath -Value $bicepConfigJson -Force
